7357ec03d765212c7527df88b311454268c186ab
Your Name <you@example.com>
2023-03-22 20:18:48 -0600

    se mandaron datos a las tablas camion y transporte en el mismo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\camionModel;
+use App\Models\transporteModel;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +20,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $transporte = new transporteModel();
+        $transporte->codigo='abc12345';
+        $transporte->nombre='proveedor3';
+        $transporte->razon_social='Entre Rios';
+        $transporte->save();
 
+        $camion = new camionModel();
+        $camion->placa_camion='abc123';
+        $camion->marca='Foton';
+        $camion->color='azul';
+        $camion->modelo='2023-03-03';
+        $camion->capacidad_toneladas='2';
+        $camion->transporte_codigo='abc12345';
+        $camion->id_placa='1';
+        $camion->save();
     }
 }
